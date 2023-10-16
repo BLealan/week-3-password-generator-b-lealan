3089c29fc27ad493a3e03b03e56daad35552720d
BLealan <143086275+BLealan@users.noreply.github.com>
2023-10-16 23:05:10 +0100

    add lower case confirm

--- script.js
+++ script.js
@@ -13,13 +13,24 @@ function generatePassword() {
 
 var input = [];
 // Asks user to input length of password
-let passwordLength = prompt("How long would you like your password to be");
+var passwordLength = prompt("How long would you like your password to be");
 
 // If password length is outside acceptance criteria it will ask for another number
 while (passwordLength < 8 || passwordLength > 128) {
   passwordLength = prompt("Please pick a number between 8 and 128")
 }
 console.log (passwordLength);
+
+// Asks user if they want password to contain lower case characters
+var lowerCase = confirm("Would you like lower case characters?")
+
+// If the confirm lowerCase variable saved as true, otherwise it is saved as false
+if (lowerCase) {
+  lowerCase = true;
+} else {
+  lowerCase = false;
+}
+console.log (lowerCase);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
